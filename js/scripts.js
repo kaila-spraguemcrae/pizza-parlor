f402a44d37bb353fb723b91701dbf72764fe59c2
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-11-06 15:43:54 -0800

    add two values for inputted priced items and pushed them to different variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,34 @@
 //bussiness logic
+// function Order {
+//   this.pizzas = [];
+// }
 
-function PizzaOrder (firstName, lastName, size) {
+function PizzaOrder (firstName, lastName, size, sizePrice) {
   this.firstName = firstName;
   this.lastName = lastName
   this.size = size;
-  this.toppings = []; //the parseFloated values will be pushed to array
+  this.sizePrice = sizePrice;
+  this.toppingsPrice = []; 
+  this.toppingsName = [];
   this.price = 0;
 }
 
-PizzaOrder.prototype.addSize = function(num) {
-  this.size = num
+PizzaOrder.prototype.addSizePrice = function(num) {
+  this.sizePrice = num
 }
 
 PizzaOrder.prototype.addToppings = function() {
-  for(i=0; i < toppings.length; i++) {
-    this.toppings.push(toppings[i]);
+  for(i=0; i < toppingsPrice.length; i++) {
+    this.toppingsPrice.push(toppingsPrice[i]);
+    this.toppingsName.push(toppingsName[i]);
   }
-  let sum = this.toppings.reduce(function(a, b){
+  let sum = this.toppingsPrice.reduce(function(a, b){
     return a + b});
-  this.toppings = sum;
+  this.toppingsPrice = sum;
 }
 
 PizzaOrder.prototype.totalPrice = function() {
-  let result = this.size + this.toppings;
+  let result = this.sizePrice + this.toppingsPrice;
   this.price = Math.round(result * 100)/ 100;
 }
 
@@ -40,30 +46,45 @@ PizzaOrder.prototype.totalPrice = function() {
 // newOrder.totalPrice();
 
 //user interface logic
-let toppings = [];
+let toppingsPrice = [];
+let toppingsName= []; 
+
+// function displayOrderDetails()
+function splitSize() {
+
+}
 
 $(document).ready(function(){
 
-  
   $("form#order").submit(function(event){
     event.preventDefault();
     const inputtedFirstName = $("input#first-name").val();
     const inputtedLastName = $("input#last-name").val();
-    const inputtedSize= parseFloat($("input[name='size']:checked").val());
+
+    
+    const sizeinput = $("input[name='size']:checked").val();
+    console.log(sizeinput);
+    const splitArr = sizeinput.split(" ");
+    console.log(splitArr);
+    const inputtedSize = splitArr[0];
+    const inputtedSizePrice = parseFloat(splitArr[1]);
+   console.log(splitArr[1]);
     
     $("input:checkbox[name=toppings]:checked").each(function(){
-      const toppingChoice = parseFloat($(this).val());
-      toppings.push(toppingChoice);
+      const toppingChoice = $(this).val();
+      const splitToppings = toppingChoice.split(" ");
+      toppingsName.push(splitToppings[0]);
+      toppingsPrice.push(parseFloat(splitToppings[1]));
     });
-    let newOrder = new PizzaOrder (inputtedFirstName, inputtedLastName, inputtedSize);
+    let newOrder = new PizzaOrder(inputtedFirstName, inputtedLastName, inputtedSize, inputtedSizePrice);
+
     newOrder.addToppings();
-    newOrder.addSize(inputtedSize);
+    newOrder.addSizePrice(inputtedSizePrice);
     newOrder.totalPrice();
+    
     console.log(newOrder);
-    console.log(newOrder.toppings);
-    console.log(newOrder.price);
+    
   });
   
-
 });
 
